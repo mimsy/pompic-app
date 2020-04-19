648e2d031635a14857ec96213245428d189af439
James Dewar <james@arcs.in>
2020-04-19 10:58:59 -0400

    very basic app

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>text dreams</Text>
+              <Text style={styles.sectionDescription}>
+                write your <Text style={styles.highlight}>text</Text> here.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>audio dreams</Text>
+              <Text style={styles.sectionDescription}>
+                record your <Text style={styles.highlight}>audio</Text> here.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>visual dreams</Text>
+              <Text style={styles.sectionDescription}>
+                draw your <Text style={styles.highlight}>sketch</Text> here.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>your dreams</Text>
+              <Text style={styles.sectionDescription}>
+                view your <Text style={styles.highlight}>dreams</Text> here.
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'lightgray',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'darkgray',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: 'darkgray',
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'pompic-dreams',
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://x3vxwj544m.execute-api.us-east-1.amazonaws.com/dev/',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_OaI8mjlOH',
+    APP_CLIENT_ID: '5peml0trda50jjaherkm161hmm',
+    IDENTITY_POOL_ID: 'us-east-1:8f032c70-963e-4eb1-9802-6e43fb154c14',
+  },
+};
